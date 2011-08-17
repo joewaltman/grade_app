629d03c905cf8c0cce4023db8f4debda66d228e7
Joe Waltman <joewaltman@gmail.com>
2011-08-17 10:35:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GradeApp::Application.config.secret_token = '1e7d57d0595afeab1c6f3663257be6fb923948359d71e5ac5a9255204bea09b8f9726410e34ce147b65e9ef4bf3b2475349d7a99e705177c438146209618bb7a'
