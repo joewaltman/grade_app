370b1c8f4c28cb51b7e83063ada0cae90822ddd1
Joe Waltman <joewaltman@gmail.com>
2011-08-17 14:51:29 -0700

    Added a site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def about
+    @title = "About"
   end
 
   def privacy
+    @title = "Privacy"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,25 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", 
+                                    :content => "Pay4Grades | Home")
+    end
+    
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+    
   end
 
   describe "GET 'about'" do
@@ -14,6 +27,13 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", 
+                                    :content => "Pay4Grades | About")
+    end
+    
   end
 
   describe "GET 'privacy'" do
@@ -21,6 +41,13 @@ describe PagesController do
       get 'privacy'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'privacy'
+      response.should have_selector("title", 
+                                    :content => "Pay4Grades | Privacy")
+    end
+    
   end
 
 end
