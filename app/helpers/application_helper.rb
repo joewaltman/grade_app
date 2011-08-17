0f621d80333d41322270a875ff2f83b541bf9eeb
Joe Waltman <joewaltman@gmail.com>
2011-08-17 15:09:50 -0700

    Added a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # Return a title on  per-page basis
+  def title
+    base_title = "Pay4Grades"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
 end
