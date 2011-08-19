19971860bff08a8e9d5d03a082128f7e583a92ed
Joe Waltman <joewaltman@gmail.com>
2011-08-18 18:41:49 -0700

    Added password and encrypted password attributes to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,26 @@
 
-#id         :integer
-#name       :string
-#email      :string
-#dob        :date
-#created_at :datetime
-#updated_at :datetime
+#id               :integer
+#name             :string
+#email            :string
+#dob              :date
+#created_at       :datetime
+#updated_at         :datetime
+#encrypted_password :string
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :dob
+  attr_accessor   :password
+  attr_accessible :name, :email, :dob, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
-  validates :name,    :presence => true, 
-                      :length => { :maximum => 50 }
-  validates :email,   :presence => true, 
-                      :format => { :with => email_regex }, 
-                      :uniqueness => {:case_sensitive => false}
+  validates :name,      :presence => true, 
+                        :length => { :maximum => 50 }
+  validates :email,     :presence => true, 
+                        :format => { :with => email_regex }, 
+                        :uniqueness => {:case_sensitive => false}
+  validates :password,  :presence => true,
+                        :confirmation => true,
+                        :length => { :within => 6..40 }
+  
                       
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110818214105) do
+ActiveRecord::Schema.define(:version => 20110819013635) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110818214105) do
     t.date     "dob"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
