4af68f229e5610c272f3f88a2eb3fda0949b1b42
Joe Waltman <joewaltman@gmail.com>
2011-08-19 10:01:16 -0700

    Improved the show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,6 +18,16 @@ describe UsersController do
       get :show, :id => @user
       assigns(:user).should == @user
     end
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector('title', :content => @user.name)
+    end
+    
+    it "should have the user's name" do
+      get :show, :id => @user
+      response.should have_selector('h1', :content => @user.name)
+    end
+    
   end
 
   describe "GET 'new'" do
@@ -30,6 +40,8 @@ describe UsersController do
       get :new
       response.should have_selector('title', :content => "Sign up")
     end
+
+    
   end
 
 end
