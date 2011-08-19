4816dceafd964c59936e4103f7335d5ce6b17705
Joe Waltman <joewaltman@gmail.com>
2011-08-19 09:45:29 -0700

    added a user factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Joe Waltman"
+  user.email                  "joe@example.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
