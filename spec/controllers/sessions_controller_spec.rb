6205f7bdf7bb77b984437828e413c36b20d6084b
Joe Waltman <joewaltman@gmail.com>
2011-08-21 18:25:01 -0700

    Handled invalid signups

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -8,7 +8,21 @@ describe SessionsController do
       get :new
       response.should be_success
     end
+  end
+  
+  describe "POST 'create'" do
     
+    describe "failure" do
+      
+      before(:each) do
+        @attr = {:email => "", :password => ""}
+      end
+      
+      it "should re-render the new page" do
+        post :create, :session => @attr
+        response.should render_template('new')
+      end
+    end
   end
 
 end
